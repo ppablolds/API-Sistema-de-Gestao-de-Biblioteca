5eacc073106fe53d1b141bccafb7e8f9107cbe17
ppablolds <pablosilvaaluno@outlook.com>
2025-06-04 16:04:54 -0300

    feat: Create DTOs AutorRequestDto and AutorResponseDto

--- src/main/java/com/biblioteca/api/dto/autor/AutorRequestDto.java
+++ src/main/java/com/biblioteca/api/dto/autor/AutorRequestDto.java
@@ -1,4 +1,25 @@
 package com.biblioteca.api.dto.autor;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class AutorRequestDto {
+
+    @NotBlank
+    @Size(max = 100, message = "O nome do autor deve ter no máximo 100 caracteres.")
+    private String nomeAutor;
+
+    public AutorRequestDto() {}
+
+    public AutorRequestDto(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
 }
--- src/main/java/com/biblioteca/api/dto/autor/AutorResponseDto.java
+++ src/main/java/com/biblioteca/api/dto/autor/AutorResponseDto.java
@@ -1,4 +1,30 @@
 package com.biblioteca.api.dto.autor;
 
 public class AutorResponseDto {
+
+    private Long id;
+    private String nomeAutor;
+
+    public AutorResponseDto() {}
+
+    public AutorResponseDto(Long id, String nomeAutor) {
+        this.id = id;
+        this.nomeAutor = nomeAutor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
 }
