d7f4e5ffdd44f075ea5da619302ce010bc42ab1b
ppablolds <pablosilvaaluno@outlook.com>
2025-06-06 17:41:11 -0300

    feat: Config security filter

--- src/main/java/com/biblioteca/api/config/security/WebSecurityConfig.java
+++ src/main/java/com/biblioteca/api/config/security/WebSecurityConfig.java
@@ -2,14 +2,30 @@ package com.biblioteca.api.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Desabilita a proteção CSRF (necessário para Postman sem tokens)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Permite todas as requisições sem autenticação
+                );
+        return http.build();
     }
 }
